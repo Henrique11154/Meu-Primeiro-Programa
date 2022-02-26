e6042186e65bc08414c95cc4a35140cc8f14c181
Henrique <henrique65.cass@gmail.com>
2022-02-26 17:13:43 -0300

    Atividade1

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class MeuPrimeiroPrograma{
+
+	public static void main(String args[]){
+		 System.out.println("Laboratorio de Programação 2 - LP2");
+		 System.out.println("2 Ano informática");
+		 System.out.println("Seja bem vindo");
+		 System.out.println("Vamos aprender java.");
+	}
+}
